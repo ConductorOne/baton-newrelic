dbd19165f5318ae28fe3457ff023946d11be5877
Martin Albert <martin.albert.187@gmail.com>
2023-11-20 15:11:31 +0100

    Prepare CLI

--- cmd/baton-newrelic/config.go
+++ cmd/baton-newrelic/config.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/spf13/cobra"
 )
 
 // config defines the external configuration required for the connector to run.
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
+	APIKey         string                   `mapstructure:"apikey"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
+	if cfg.APIKey == "" {
+		return fmt.Errorf("apikey must be provided")
+	}
+
 	return nil
+}
+
+func cmdFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().String("apikey", "", "The API key used to connect to NewRelic GraphQL API. ($BATON_APIKEY)")
 }
