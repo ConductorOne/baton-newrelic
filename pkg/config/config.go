3c226935c1bb76aca5e7fd783896ad92cab95765
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:06:21 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,6 +19,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the NewRelic API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	// FieldRelationships defines relationships between the fields listed in
